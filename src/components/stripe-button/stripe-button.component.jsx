85e87a36fd4556762b673084a938b91252ebe645
lebashir <bashir.abihabib@gmail.com>
2021-05-28 14:51:15 -0500

    fixed typo

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -13,7 +13,7 @@ const StripeCheckoutButton = ({ price }) => {
     return (
         <StripeCheckout 
             label='Pay Now'
-            name= 'Crown Clothin'
+            name= 'Crown Clothing'
             billingAddress
             shippingAddress
             image ='https://svgshare.com/i/CUz.svg'
